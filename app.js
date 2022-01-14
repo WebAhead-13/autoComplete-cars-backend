af0579b10b04cf1a76904c4fbd06b3ee668989e4
“Riham <re.khalaily@gmail.com>
2022-01-14 21:40:57 +0200

    last style

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function layout(content) {
       </head>
       <body>
         <nav>
-        <a href="/">Home</a>
+        <a id= "home" class="homeButton" href="/">Home</a>
         <a id= "login" class="loginbutton" href="/log-in">log-in</a>
         <a id= "logout" class="logoutbutton" href="/log-out">log-out</a>
         </nav>
@@ -23,22 +23,35 @@ function layout(content) {
 }
 function home() {
   return layout(`
-  <cards></cards>
-    <h1>Know your CAR</h1>
-        <p>
-        The platform will provide you selected information and an image,
-        please type in the hereby provided search bar, start typing to view your matching car options.
-        Furthermore an extra GIF will be provided below as a nice extra touch.
-        </p>
+  <cards></cards> 
+  <div class="content">
+    <h1 ><b>F</b>ind Your Car</h1>
+    
+    <p><p><p>The platform will provide you selected information and an image,<br>
+    please type in the hereby provided search bar, start typing to view<br> 
+    your matching car options.<br>
+    Furthermore an extra GIF will be provided below as a nice extra touch.</p></p>
+    
+    </p><br>
+    </div>
+    <div class="image">
+    <img src="https://media.istockphoto.com/photos/electric-sports-car-struck-by-electrical-lightning-from-coil-picture-id1291903867?b=1&k=20&m=1291903867&s=170667a&w=0&h=gOMV4rq4BUZyqX9ICOujnw0VMv8-RPzQn2PwJF8MrWE=">
+
+    </div>
+<br>
+<br>
+    
+       <div class="buttonStyle">
         <form>
           <label for="car" id="car_label" name="car_label">choose a car</label>
 
           <input type="text" placeholder="example: bmw" id="car" name="car" onkeyup="keyUp()" list="carList"/>
           <datalist id="carList"> 
           </datalist>
-          <input type="submit" value="Search" id="submit" required/>
+          <input class="searchbutton" type="submit" value="Search" id="submit" required/>
           <output></output>
         </form>
+        </div>
   `);
 }
 function logIn() {
